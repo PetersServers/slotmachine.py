8ca00889ddcc65abc1884cf2ef715fa91f8c49dc
Peter Pichler <csav9172@student.uibk.ac.at>
2023-01-12 21:01:50 +0000

    Changes by Online Editor

--- slot_machine.py
+++ slot_machine.py
@@ -9,3 +9,13 @@ SYMBOLS = ('A', 'B', 'C', 'D', 'E')
 
 def _get_symbol():
     return random.choice(SYMBOLS)
+    
+def _get_different_symbols():
+    symbol1 = _get_symbol()
+    symbol2 = _get_symbol()
+    symbol3 = _get_symbol()
+    while symbol1 == symbol2 == symbol3:
+        symbol1 = _get_symbol()
+        symbol2 = _get_symbol()
+        symbol3 = _get_symbol()
+    return (symbol1, symbol2, symbol3)
