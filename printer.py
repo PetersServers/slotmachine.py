4edd423b6457c6649045edaf6e167edc07dc3fbf
Peter Pichler <csav9172@student.uibk.ac.at>
2023-01-12 20:58:16 +0000

    Changes by Online Editor

--- printer.py
+++ printer.py
@@ -14,3 +14,13 @@ def _print_slots(symbols):
     for symbol in symbols:
         print(f"[{symbol}]", end=" ")
     print()
+def _print_text(text):
+    print("+------------+")
+    print("|  " + text + "  |")
+    print("+------------+")
+
+def print_machine(symbols, text):
+    _print_top()
+    _print_slots(symbols)
+    _print_base()
+    _print_text(text)
